ccab92e84e5b3b0901baf79ac5450fa3586fb3b8
AnDrobysh <lonely21shadow21112>
2020-12-28 14:33:01 +0300

    task 3

--- task 3.py
+++ task 3.py
@@ -1,3 +1,10 @@
+def fibonacci(a):
+    z = 1
+    if a > 2:
+        z = fibonacci(a-1) + fibonacci(a-2)
+    return z
+
+
 a = int(input('введите номер числа из последовательности Фибонначи'))
 
 
@@ -5,10 +12,11 @@ c = [1]
 i = 1
 x = 1
 
-while i < a+1:
+while i < a:
     c.insert(i, x)
     x = c[i] + c[i-1]
     i = i+1
     print(c)
 
-print(c[a])
+print(fibonacci(a))
+print(c[a-1])
