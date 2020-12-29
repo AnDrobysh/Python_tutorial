0cb0f98bd6c63d023aedd9e6600efee6df285008
AnDrobysh <lonely21shadow21112>
2020-12-29 11:37:20 +0300

    Refactoring task  2

--- task_2.py
+++ task_2.py
@@ -1,17 +1,27 @@
-string = input("введите строку")
+input_string = input("введите строку")
 
-string.lower()
+input_string.lower()
 
-a, c = [], []
 
-for number in string:
-    c.append(number)
+# a, c = [], []
+#
+# for number in string:
+#     c.append(number)
+#
+# a = c.copy()
+#
+# a.reverse()
+#
+# if c == a:
+#     print("Это палиндорм")
+# else:
+#     print("Это не палиндорм")
 
-a = c.copy()
+def is_palindrome(string):
+    if string == ''.join(reversed(string)):
+        return 'palindrom'
+    else:
+        return 'not_palindrom'
 
-a.reverse()
 
-if c == a:
-    print("Это палиндорм")
-else:
-    print("Это не палиндорм")
+print(is_palindrome(input_string))
