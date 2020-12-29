17e3d61c892a4fddc4acb3bda87b00a42aee1e00
AnDrobysh <lonely21shadow21112>
2020-12-29 15:36:34 +0300

    task_4 "instance"

--- task_4.py
+++ task_4.py
@@ -1,39 +1,58 @@
 a = [1, 2, 3, [2, 34, [12, {'DO': 1, 'THE': 2}, 19]], (1, 2, 3), "Kek"]
-c = []
 
-def pars(a):
-    b = []
-    for number in a:
-        if type(number) == int:
-            a.append(number)
-            a.remove(number)
-        elif type(number) == str:
-            a.append(number)
-            a.remove(number)
-        elif type(number) == float:
-            a.append(number)
-            a.remove(number)
-        if type(number) == tuple:
-            number_list = number
-            a.remove(number)
-            for numbers_of_number_list in number_list:
-                a.append(numbers_of_number_list)
-                pars(a)
-        elif type(number) == dict:
-            number_list = number
-            a.remove(number)
-            for numbers_of_number_list in number_list:
-                a.append(numbers_of_number_list)
-                a.append(number_list[numbers_of_number_list])
-                pars(a)
-        elif type(number) == list:
-            number_list = number
-            a.remove(number)
-            for numbers_of_number_list in number_list:
-                a.append(numbers_of_number_list)
-                pars(a)
-    return a
 
-pars(a)
-print(a)
+def make_lin(m_list):
+    n_list = []
+    for x in m_list:
+        if isinstance(x, (list, tuple, dict)):
+            if isinstance(x, (list, tuple)):
+                n_list += make_lin(x)
+            else:
+                n_list += make_lin(x.items())
+        else:
+            n_list.append(x)
+    return n_list
 
+
+print(make_lin(a))
+
+
+# a = [1, 2, 3, [2, 34, [12, {'DO': 1, 'THE': 2}, 19]], (1, 2, 3), "Kek"]
+# # c = []
+# #
+# #
+# # def pars(pars_list):
+# #     for number in pars_list:
+# #         if type(number) == int:
+# #             pars_list.append(number)
+# #             pars_list.remove(number)
+# #         elif type(number) == str:
+# #             pars_list.append(number)
+# #             pars_list.remove(number)
+# #         elif type(number) == float:
+# #             pars_list.append(number)
+# #             pars_list.remove(number)
+# #         if type(number) == tuple:
+# #             number_list = number
+# #             pars_list.remove(number)
+# #             for numbers_of_number_list in number_list:
+# #                 pars_list.append(numbers_of_number_list)
+# #                 pars(pars_list)
+# #         elif type(number) == dict:
+# #             number_list = number
+# #             pars_list.remove(number)
+# #             for numbers_of_number_list in number_list:
+# #                 pars_list.append(numbers_of_number_list)
+# #                 pars_list.append(number_list[numbers_of_number_list])
+# #                 pars(pars_list)
+# #         elif type(number) == pars_list:
+# #             number_list = number
+# #             pars_list.remove(number)
+# #             for numbers_of_number_list in number_list:
+# #                 pars_list.append(numbers_of_number_list)
+# #                 pars(pars_list)
+# #     return pars_list
+# #
+# #
+# # pars(a)
+# # print(a)
